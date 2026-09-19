42e84149eb31735d2e1cee55265e287371161c8a
unknown <bardia490@gmail.com>
2026-09-19 20:22:21 +0330

    changed every any interface to structs for better and faster approach

--- cmd/merka/main.go
+++ cmd/merka/main.go
@@ -20,13 +20,16 @@ import (
 )
 
 func main() {
-	path := "configs/works.json"
-	db, err := database.Create(path)
+	path := "configs/example_works.json"
+	db, err := database.Create(path) // db is a pointer to the database struct
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 	db.PrintFullDataBase()
+	if err := db.PrintWork("goshvare_mosalasi2"); err != nil {
+		fmt.Println("couldn't print work")
+	}
 	if err = db.SaveDataBaseToFile(); err != nil {
 		fmt.Println(err.Error())
 	}
--- internals/database/main.go
+++ internals/database/main.go
@@ -8,15 +8,70 @@ import (
 	"os"
 )
 
+type PriceKind int // to I can diffrentiate between an actual price value and an PriceTag
+const (
+	PriceValue PriceKind = iota
+	PriceTag
+)
+
+// price can either be an float64 or a "price tag" which is an string
+type Price struct {
+	Kind  PriceKind
+	Value float64 // for PriceKind == PriceValue
+	Tag   string  // for PriceKind == PriceTag
+}
+
+// the items each work should be able to hold
+type Work struct {
+	Monjogs   map[string]int `json:"monjogs"`
+	Materials map[string]int `json:"materials"`
+	Time      Price          `json:"times"`
+}
+
+// Implement the Unmarshal interface so i can read Price values easier
+func (price *Price) UnmarshalJSON(b []byte) error {
+	var num float64
+	if err := json.Unmarshal(b, &num); err == nil {
+		price.Kind = PriceValue
+		price.Value = num
+		price.Tag = ""
+		return nil
+	}
+	var tag string
+	if err := json.Unmarshal(b, &tag); err == nil {
+		price.Kind = PriceTag
+		price.Value = 0
+		price.Tag = tag
+		return nil
+	}
+	return fmt.Errorf("price must be a number or a string tag, got: %s", string(b))
+}
+
+// Implement the Unmarshal interface so i can write Price values easier
+func (p Price) MarshalJSON() ([]byte, error) {
+	switch p.Kind {
+	case PriceValue:
+		return json.Marshal(p.Value)
+	case PriceTag:
+		return json.Marshal(p.Tag)
+	default:
+		return nil, fmt.Errorf("unknown price kind: %v", p.Kind)
+	}
+}
+
 // DataBase
 type DB struct {
-	data map[string]any
+	Works            map[string]Work  `json:"works"`
+	Time             map[string]Price `json:"time"`
+	OtherMaterials   map[string]Price `json:"other_materials"`
+	Codes            map[string]Price `json:"codes"`
+	Additional_costs map[string]Price `json:"additional_costs"`
+	PriceTags        map[string]Price `json:"price tags"`
 }
 
 // the [path] is the relative path to the configs\works.json file
-func Create(path string) (DB, error) {
+func Create(path string) (*DB, error) {
 	var db DB
-	db.data = make(map[string]any, 10)
 
 	if !utility.DoesFileExist(path) {
 		path = "configs/work_template.json"
@@ -24,41 +79,79 @@ func Create(path string) (DB, error) {
 
 	contents, err := os.ReadFile(path)
 	if err != nil {
-		return DB{}, err
+		return nil, err
 	}
-	err = json.Unmarshal([]byte(contents), &db.data)
+	err = json.Unmarshal([]byte(contents), &db)
 	if err != nil {
-		return DB{}, err
+		return nil, err
 	}
-	return db, nil
+	return &db, nil
 }
 
-func (db DB) PrintFullDataBase() {
-	printJson(db.data)
+// prints all the works names + monjog + material + time costs
+func (db *DB) PrintFullDataBase() {
+	printWorks(db.Works)
 }
 
-func printJson(data map[string]any) {
-	if len(data) == 0 {
+// this should be used when we want to print a work based on its name and we also want
+// to check if the work exists beforehand
+func (db *DB) PrintWork(workname string) error {
+	// if the work existed inside the database
+	if work, ok := db.Works[workname]; ok {
+		fmt.Println("work:", workname)
+		work.Print()
+		return nil
+	}
+	return fmt.Errorf("could not find work: %s in database", workname)
+}
+func printWorks(works map[string]Work) {
+	if len(works) == 0 {
 		fmt.Println("there was no data here!")
+		return
 	}
-	for key, val := range data {
-		switch vv := val.(type) {
-		case string:
-			fmt.Println(key, "is string", vv)
-		case float64:
-			fmt.Println(key, "is float64", vv)
-		case []any:
-			fmt.Println(key, "is an array:")
-			for i, u := range vv {
-				fmt.Println(i, u)
-			}
-		case map[string]any: // for objects (maps in go)
-			fmt.Println(key, "is an map with contents:")
-			printJson(vv)
-		default:
-			fmt.Println(key, "is of a type I don't know how to handle")
+	for key, work := range works {
+		fmt.Printf("work: %s\n", key)
+		work.Print()
+	}
+}
+
+// if a work exists inside the database we can use this function to print its
+// monjogs, materials and time (if unsure about the existence of the work, should use DataBase.PrintWork function)
+func (work *Work) Print() {
+	work.printMonjogs()
+	work.printMaterials()
+	work.printTime()
+}
+
+func (work *Work) printMonjogs() error {
+	// NOTE: this needs to change cause currently I
+	// don't access to the price tags from the database
+	//Monjogs  map[string]Price
+	if len(work.Monjogs) != 0 {
+		fmt.Println("monjogs:")
+		for m_name, m_count := range work.Monjogs {
+			fmt.Printf("monjog code: %s, monjog count: %f\n", m_name, m_count)
+		}
+	} else {
+		fmt.Println("there were no monjogs in this work!!, please make sure this was intentional")
+	}
+	return nil
+}
+
+func (work *Work) printMaterials() {
+	// NOTE: this needs to change cause currently I
+	// don't access to the price tags from the database
+	//Materials map[string]float64
+	if len(work.Materials) != 0 {
+		fmt.Println("materials:")
+		for m_name, m_price := range work.Materials {
+			fmt.Printf("material name: %s, material price: %d\n", m_name, m_price)
 		}
 	}
+}
+func (work *Work) printTime() {
+	//Time map[string]Price
+	fmt.Printf("total time for work: %.2f\n", work.Time)
 }
 
 func (db *DB) Reload() error {
@@ -71,11 +164,26 @@ func (db *DB) Reload() error {
 	if err != nil {
 		return err
 	}
-	err = json.Unmarshal([]byte(contents), &db.data)
+	err = json.Unmarshal([]byte(contents), db)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+// every price must be converted into an float64
+// this method will find the correct tag value (if the type is an price tag)
+// or the value (if the type is an value from the begining), it will also convert
+// every -1 into the default price value
+// if none of the above matches it will return an error
+func (price *Price) resolve() (float64, error) {
+	switch price.Kind {
+	case PriceValue:
+		return price.Value, nil // NOTE: Add -1 check for default value
+	case PriceTag:
+		return 100, nil // NOTE: FIX this
+	}
+	return 0, fmt.Errorf("could not resolve the price: %v. please contact your boyfriend immediatley", *price)
 }
 
 func (db *DB) SaveDataBaseToFile() error {
@@ -86,77 +194,9 @@ func (db *DB) SaveDataBaseToFile() error {
 	}
 	defer file.Close()
 
-	if err := json.MarshalWrite(file, db.data, jsontext.WithIndent("  ")); err != nil {
+	if err := json.MarshalWrite(file, *db, jsontext.WithIndent("  ")); err != nil {
 		return fmt.Errorf("failed to encode json: %w", err)
 	}
 
 	return nil
-}
-
-func (db DB) PrintWork(workname string) error {
-	// if the work existed inside the database
-	if _, ok := db.data[workname]; !ok {
-		return fmt.Errorf("could not find work: %s in database", workname)
-	}
-	work_contents, work_assertion_ok := db.data[workname].(map[string]any)
-	if !work_assertion_ok { // if work wasn't map[string]any for some wierd reason
-		return fmt.Errorf("PLEASE CALL YOU'RE BOYFRIEND IMMIEDIETLY, work %s wasn't an map[string]any, it was: %v", workname, db.data[workname])
-	}
-	// check to see if monjogs are in work
-	if monjogs, ok := work_contents["monjogs"]; ok {
-		monjogs_contents, monjogs_assertion_ok := monjogs.(map[string]any)
-		if !monjogs_assertion_ok { // if monjogs wasn't map[string]any for some wierd reason
-			return fmt.Errorf("PLEASE CALL YOU'RE BOYFRIEND IMMIEDIETLY, monjogs wasn't an map[string]any in work: %s, it was: %v", workname, monjogs)
-		}
-		db.printMonjogs(monjogs_contents)
-	}
-	// check to see if materials are in work
-	if materials, ok := work_contents["materials"]; ok {
-		materials_contents, materials_assertion_ok := materials.(map[string]any)
-		if !materials_assertion_ok { // if materials wasn't map[string]any for some wierd reason
-			return fmt.Errorf("PLEASE CALL YOU'RE BOYFRIEND IMMIEDIETLY, materials wasn't an map[string]any in work: %s, it was: %v", workname, materials)
-		}
-		db.printMaterials(materials_contents)
-	}
-	// check to see if time is in work
-	if times, ok := work_contents["times"]; ok {
-		times_content, times_assertion_ok := times.(float64)
-		if !times_assertion_ok { // if times wasn't map[string]any for some wierd reason
-			return fmt.Errorf("PLEASE CALL YOU'RE BOYFRIEND IMMIEDIETLY, times wasn't an float64 in work: %s, it was: %v", workname, times)
-		}
-		db.printTime(times_content)
-	}
-	return nil
-}
-
-func (db *DB) printMonjogs(monjogs map[string]any) error {
-	codes := db.data["codes"].(map[string]any) // NOTE: should not use any here, it should be float64|string, change it later
-
-	for monjog_name, count_interface := range monjogs {
-		var price float64
-
-		// how many monjogs of this type do we have
-		count := count_interface.(float64)
-
-		// find the price for this monjog
-		if price_interface, ok := codes[monjog_name]; ok {
-			switch val := price_interface.(type) {
-			case float64: // NOTE: for backwards compatibility it should check for -1 in case of default
-				price = val
-			case string:
-				fmt.Println("not implemented yet, will use 1 for now")
-				price = 1
-			default:
-				return fmt.Errorf("the price for monjog: %s was not correctly written in the database, please correct the price for this monjog in the \"codes\" part", monjog_name)
-			}
-		} else {
-			return fmt.Errorf("the price for monjog: %s was not in the database, please add the price for this monjog in the \"codes\" part", monjog_name)
-		}
-		fmt.Printf("monjog code: %s\n count: %d\n price: %f, total price: %f", monjog_name, int(count), price, count*price)
-	}
-	return nil
-}
-func (db *DB) printMaterials(materials map[string]any) {
-}
-func (db *DB) printTime(time float64) {
 }
