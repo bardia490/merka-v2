fb2f70ef2842d13d2cd8b0df371d95eb6d63c7b5
unknown <bardia490@gmail.com>
2026-09-22 21:23:25 +0330

    changed the internal work printing function for easier price calculations

--- cmd/merka/main.go
+++ cmd/merka/main.go
@@ -26,7 +26,7 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
-	//database.PrintFullDataBase()
+	database.PrintFullDataBase()
 	//if err := database.PrintWork("goshvare_mosalasi2"); err != nil {
 	//	fmt.Println("couldn't print work")
 	//}
--- internals/database/main.go
+++ internals/database/main.go
@@ -41,10 +41,12 @@ func LoadDataBase(path string) error {
 
 	err = json.Unmarshal(contents, db)
 	if err != nil {
+		// check for a syntax Error
 		if syntaxErr, ok := errors.AsType[*jsontext.SyntacticError](err); ok {
 			line, col := calculateLineAndColumnJsonError(contents, syntaxErr.ByteOffset)
 			return fmt.Errorf("there was a problem whith parsing the json file at line: %d, column: %d. the ERROR: %s", line, col, err.Error())
 		}
+		// check for symantic Error
 		if semErr, ok := errors.AsType[*json.SemanticError](err); ok {
 			// SemanticError in v2 includes JSONPointer path information!
 			if semErr.ByteOffset > 0 {
@@ -56,13 +58,13 @@ func LoadDataBase(path string) error {
 		}
 		return err
 	}
-	runChecks(checkMonjogs | checkTimes | checkMaterials | checkTime | checkPrices | checkDefaultPrice)
+	//runChecks(checkMonjogs | checkTimes | checkMaterials | checkTime | checkPrices | checkDefaultPrice)
 	return nil
 }
 
 // prints all the works names + monjog + material + time costs
 func PrintFullDataBase() {
-	printWorks(db.Works)
+	printWorks(db.Works, true)
 }
 
 // this should be used when we want to print a work based on its name and we also want
@@ -71,21 +73,21 @@ func PrintWork(workname string) error {
 	// if the work existed inside the database
 	if work, ok := db.Works[workname]; ok {
 		fmt.Println("work:", workname)
-		work.Print()
+		work.print(false)
 		return nil
 	}
 	return fmt.Errorf("could not find work: %s in database", workname)
 }
 
 // prints all the works in database using the Print method for each work
-func printWorks(works map[string]Work) {
+func printWorks(works map[string]Work, verbose bool) {
 	if len(works) == 0 {
 		fmt.Println("there was no data here!")
 		return
 	}
 	for key, work := range works {
 		fmt.Printf("work: %s\n", key)
-		work.Print()
+		work.print(verbose)
 		fmt.Println("<><><><><><><><><><><><><><><><><><><><><>")
 	}
 }
@@ -93,32 +95,62 @@ func printWorks(works map[string]Work) {
 // if a work exists inside the database we can use this function to print its
 // monjogs, materials and time (if unsure about the existence of the work,
 // should use DataBase.PrintWork function)
-func (work *Work) Print() {
-	work.printMonjogs()
-	work.printMaterials()
-	work.printTime()
+// if verbose is set to true then the whole information about each monjog (including count, price and total price) is displayed
+func (work *Work) print(verbose bool) {
+	total_price, monjog_price, material_price, time_price := 0.0, 0.0, 0.0, 0.0
+	var err error
+	// print the complete monjogs prices and counts
+	if monjog_price, err = work.calcMonjogPrices(verbose); err != nil {
+		fmt.Printf("ERROR: there was a problem can calculating the price of the monjogs, please correct the mistake and try again later. the error: %s.\n Aborting Operation\n", err.Error())
+		return
+	}
+	total_price += monjog_price
+	// print the materials
+	if material_price, err = work.calcMaterialPrices(verbose); err != nil {
+		fmt.Printf("ERROR: there was a problem can calculating the price of the materials, please correct the mistake and try again later. the error: %s.\n Aborting Operation\n", err.Error())
+		return
+	}
+	total_price += material_price
+	// print the time
+	if time_price, err = work.calcTimePrice(verbose); err != nil {
+		fmt.Printf("ERROR: there was a problem can calculating the price of the time, please correct the mistake and try again later. the error: %s.\n Aborting Operation\n", err.Error())
+		return
+	}
+	total_price += time_price
 }
 
-func (work *Work) printMonjogs() error {
-	//Monjogs  map[string]Price
-	if len(work.Monjogs) != 0 {
-		fmt.Println("monjogs:")
-		for m_code, m_count := range work.Monjogs {
-			p := db.Codes[m_code]
-			price, err := p.resolve()
-			if err != nil {
-				fmt.Println("there was a problem with calculating the price of the monjogs")
-				return err
-			}
+// calculates the total price for the monjogs in [work]
+// if [verbose] is set to true, then the price for each monjog will
+// be displayed during calculation + the total price at the end
+// it returns the final price for monjogs or an error if any of the prices could not be
+// resolved (see [resolve] function for more details on resolving prices)
+func (work *Work) calcMonjogPrices(verbose bool) (total_price float64, resolve_error error) {
+	total_price = 0
+	for m_code, m_count := range work.Monjogs {
+		p := db.Codes[m_code]
+		price, err := p.resolve()
+		if err != nil {
+			fmt.Println("there was a problem with calculating the price of the monjogs")
+			return 0, err
+		}
+		total_price += price * float64(m_count)
+		if verbose {
 			fmt.Printf("monjog code: %s| monjog count: %d| monjog price: %.2f| total price: %.2f\n", m_code, m_count, price, float64(m_count)*price)
 		}
-	} else {
-		fmt.Println("there were no monjogs in this work!!, please make sure this was intentional")
 	}
-	return nil
+	if verbose {
+		fmt.Printf("the total price for monjogs is: %.2f\n", total_price)
+	}
+	return total_price, nil
 }
 
-func (work *Work) printMaterials() error {
+// calculates the total price for the materials in [work]
+// if [verbose] is set to true, then the price for each material will
+// be displayed during calculation + the total price at the end
+// it returns the final price for materials or an error if any of the prices could not be
+// resolved (see [resolve] function for more details on resolving prices)
+func (work *Work) calcMaterialPrices(verbose bool) (total_price float64, resolve_error error) {
+	total_price = 0
 	//Materials map[string]Price
 	if len(work.Materials) != 0 {
 		fmt.Println("materials:")
@@ -127,20 +159,32 @@ func (work *Work) printMaterials() error {
 			price, err := p.resolve()
 			if err != nil {
 				fmt.Printf("there was a problem with calculating the price of the material: %s\n", m_name)
-				return err
+				return 0, err
 			}
-			fmt.Printf("material name: %s| material price: %d| material price: %f.2| total price: %.2f\n", m_name, m_count, price, float64(m_count)*price)
+			if verbose {
+				fmt.Printf("material name: %s| material price: %d| material price: %f.2| total price: %.2f\n", m_name, m_count, price, float64(m_count)*price)
+			}
+			total_price += price * float64(m_count)
+		}
+		if verbose {
+			fmt.Printf("the total price for monjogs is: %.2f\n", total_price)
 		}
 	}
-	return nil
+	return total_price, nil
 }
-func (work *Work) printTime() error {
+
+// calculates the total price for the time in [work]
+// if [verbose] is set to true, then more information about the time of work will
+// be displayed during calculation + the total price at the end
+// it returns the final price for materials or an error if the price could not be
+// resolved (see [resolve] function for more details on resolving prices)
+func (work *Work) calcTimePrice(verbose bool) (total_price float64, resolve_error error) {
 	// work.Time: float64
 	t := work.Time
 	time, err := t.resolve()
 	if err != nil {
 		fmt.Println("there was a problem with calculating the amount of the time")
-		return err
+		return 0, err
 	}
 
 	//Time map[string]Price
@@ -148,10 +192,13 @@ func (work *Work) printTime() error {
 	price, err := p.resolve()
 	if err != nil {
 		fmt.Println("there was a problem with calculating the price of the time")
-		return err
+		return 0, err
 	}
-	fmt.Printf("total time for work: %.2f| time price: %.2f| total time price: %.2f\n", time, price, time*price)
-	return nil
+	total_price = time * price
+	if verbose {
+		fmt.Printf("total time for work: %.2f| time price: %.2f| total time price: %.2f\n", time, price, time*price)
+	}
+	return total_price, nil
 }
 
 // just in case the apps file is modified during runtime, this can be used to reload everything
